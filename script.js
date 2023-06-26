6b064b0d1d232dd04985a51fdb312ae939388682
TamariPopkhadze <popkhadzetamari@gmail.com>
2023-06-26 04:32:46 +0400

    add styles

--- script.js
+++ script.js
@@ -6,7 +6,8 @@ let sun = document.querySelector('.sun');
 let continentsContainer = document.querySelector('.containerOfContinents')
 
 moon.addEventListener('click', function(){
-    body.style.backgroundColor = 'black';
+    body.style.backgroundColor = '#2B3844';
+    
     moon.style.display = 'none';
     mode.innerHTML = 'Light mode';
     sun.style.display = 'block';
@@ -41,26 +42,35 @@ sun.addEventListener('click', function(){
         
         console.log(mosuliInfo)
         for(item of mosuliInfo){
+
         let eachCountry = createCountry();
         containerOfCountries.appendChild(eachCountry)
+        let infoContainer = document.createElement('div');
+        infoContainer.classList.add('containerOfInfo');
+
         let flag = createFlag(item.flags.svg)
         eachCountry.appendChild(flag);
-         let name = createName(item.name.common)
-         eachCountry.appendChild(name);
+
+        let name = createName(item.name.common)
+        eachCountry.appendChild(name);
+
         let population = createPopulation(item.population)
-        eachCountry.appendChild(population);
+        infoContainer.appendChild(population);
+
         let region = createRegion(item.region);
-        eachCountry.appendChild(region)
+        infoContainer.appendChild(region)
+
         if (item.capital)
         {
         let capital = createCapital(item.capital[0]);
-        eachCountry.appendChild(capital)
+        infoContainer.appendChild(capital)
         }
         else{
             let x = 'Undifined'
             let capital = createCapital( x);
-            eachCountry.appendChild(capital)
+            infoContainer.appendChild(capital)
         }
+        eachCountry.appendChild(infoContainer)
         }
     } )
     function createCountry() {
@@ -71,34 +81,59 @@ sun.addEventListener('click', function(){
 
     function createFlag(urlOfFlag) {
         let flag = document.createElement('img');
+        flag.classList.add('flagStyle')
         flag.src = urlOfFlag;
         return flag;
 
     }
     
     function createName(country) {
+        
         let name = document.createElement('h2')
         name.innerText = country;
+        name.classList.add('nameOfCountry')
         return name;
 
     }
     function createPopulation(population) {
+    
+        let div = document.createElement('div')
+        div.classList.add("infoDiv");
+        let title = document.createElement('p')
+        title.innerText = 'Population:  '
+        title.classList.add('title')
+        div.appendChild(title)
         let number = document.createElement('p')
-        number.innerText = population;
-        return number;
+        number.innerText = population.toLocaleString();
+        div.appendChild(number)
+        return div;
 
         
     }
     function createRegion(region) {
+        let div = document.createElement('div')
+        div.classList.add("infoDiv");
+        let title = document.createElement('p')
+        title.classList.add('title')
+        title.innerText = 'Region:  '
+        div.appendChild(title)
         let name = document.createElement('p')
         name.innerText = region;
-        return name;
+        div.appendChild(name)
+        return div;
 
     }
 function createCapital(city) {
+    let div = document.createElement('div')
+        div.classList.add("infoDiv");
+        let title = document.createElement('p')
+        title.classList.add('title')
+        title.innerText = 'Capital:  '
+        div.appendChild(title)
     let capital = document.createElement('p')
     capital.innerText = city;
-    return capital;
+    div.appendChild(capital)
+    return div;
 
 }
   
